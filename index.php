3335d3488d5db33299cb764c03074a444d254ea1
Nick <bleepbloop117@gmail.com>
2016-02-20 18:05:33 -0500

    updates to weather script, split to weather.js

--- index.php
+++ index.php
@@ -6,31 +6,12 @@
     <link rel="stylesheet" href="css/weather-icons.min.css">
   </head>
   <body>
-    <div class="weather-temp"></div><div class="weather-temp-icon"></div>
+    <div class="weather-temp"></div>
+    <div class="weather-temp-icon"></div>
+    <div class="weather-temp-high"></div>
+    <div class="weather-temp-low"></div>
 
     <script src="js/lib/jquery-1.12.0.min.js"></script>
-
-    <script>
-      function weather() {
-        var apiKey = 'a35348ed01d607d9d4743b250e58d533';
-        var url = 'https://api.forecast.io/forecast/';
-        //lat and long laconia, nh
-        var lati = 43.5275;
-        var longi = 71.4703;
-        var data;
-
-        $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?", function(data) {
-
-          //Current temp
-          var forecastIOTemp = Math.round(data.currently.temperature);
-          $('.weather-temp').html(forecastIOTemp + "°");
-
-          //Current weather icon
-          $('.weather-temp-icon').html('<i class="wi wi-forecast-io-'+ data.currently.icon +'"></i>');
-
-        });
-      }
-      weather();
-    </script>
+    <script src="js/weather.js"></script>
   </body>
 </html>
---
+++ js/weather.js
@@ -0,0 +1,27 @@
+$(document).ready(function($) {
+  function weather() {
+    var apiKey = 'a35348ed01d607d9d4743b250e58d533';
+    var url = 'https://api.forecast.io/forecast/';
+    //lat and long laconia, nh
+    var lati = 43.5275;
+    var longi = 71.4703;
+    var data;
+
+    $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?", function(data) {
+      //get weekly > daily data
+      var forecastioJson = data.daily.data[0];
+      //get current data
+      var currentTemp = data.currently.temperature;
+      var currentIcon = data.currently.icon;
+      //round temp
+      var forecastIOTemp = Math.round(currentTemp);
+      $('.weather-temp').html("Currently " + forecastIOTemp + "°");
+      //Current weather icon
+      $('.weather-temp-icon').html('<i class="wi wi-forecast-io-'+ currentIcon +'"></i>');
+      //
+      $('.weather-temp-high').html("High " + Math.round(forecastioJson.temperatureMax) + "°");
+      $('.weather-temp-low').html("Low " + Math.round(forecastioJson.temperatureMin) + "°");
+    });
+  }
+  weather();
+});
