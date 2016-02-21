3c38429a2f782c7fedc59a4d3affe7a54286efad
Nick <bleepbloop117@gmail.com>
2016-02-20 22:11:13 -0500

    start of mail script for messages
    added in a script that scrapes a mail server inbox for new messages and
    displays the body of the email on the page. need to refine and pull
    most recent messages in first.

--- index.php
+++ index.php
@@ -7,6 +7,37 @@
   </head>
   <body>
 
+
+    <?php
+      $imap = imap_open("{mail.webfaction.com:143}INBOX", "mom_swissfish", "mommirror");
+      $message_count = imap_num_msg($imap);
+
+    for ($i = 1; $i <= $message_count; ++$i) {
+        $header = imap_header($imap, $i);
+        $body = imap_fetchbody($imap, $i, "1.1");
+
+        if ($body == "") {
+            $body = imap_fetchbody($imap, $i, "1");
+        }
+
+        $body = trim(substr(quoted_printable_decode($body), 0, 100));
+
+        $prettydate = date("jS F Y", $header->udate);
+
+        if (isset($header->from[0]->personal)) {
+            $personal = $header->from[0]->personal;
+        } else {
+            $personal = $header->from[0]->mailbox;
+        }
+
+        $email = "$personal <{$header->from[0]->mailbox}@{$header->from[0]->host}>";
+        echo "On $prettydate, $email said \"$body\".\n";
+    }
+
+    imap_close($imap);
+?>
+
+
     <div class="date-day"></div>
 
     <div class="weather-temp"></div>
