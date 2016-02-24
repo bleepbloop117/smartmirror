90b5b3bdbaca3654f62db22fb6df30ac5d797423
Nick <bleepbloop117@gmail.com>
2016-02-24 08:41:44 -0500

    prettify index

--- index.php
+++ index.php
@@ -19,36 +19,35 @@
   foreach ($messages as $message) {
     //search for unread messages
     if (imap_search($imap,'UNSEEN')) {
-    //Set messages to read
-    imap_setflag_full($imap, $message, "\\Seen \\Flagged");
-    //Pull out header and body content
-    $header = imap_headerinfo($imap, $message);
-    $body = imap_fetchbody($imap, $message, "1.1");
-    //Body email backup
-    if ($body == "") {
+      //Set messages to read
+      imap_setflag_full($imap, $message, "\\Seen \\Flagged");
+      //Pull out header and body content
+      $header = imap_headerinfo($imap, $message);
+      $body = imap_fetchbody($imap, $message, "1.1");
+      //Body email backup
+      if ($body == "") {
         $body = imap_fetchbody($imap, $message, "1");
-    }
-    // Set timezone
-    date_default_timezone_set('America/New_York');
-    //Get time from email header
-    $unixdate = $header->udate;
-    //Prettify date
-    $prettydate = date("F jS", $unixdate);
-    //Get name or mailbox name is none
-    if (isset($header->from[0]->personal)) {
+      }
+      // Set timezone
+      date_default_timezone_set('America/New_York');
+      //Get time from email header
+      $unixdate = $header->udate;
+      //Prettify date
+      $prettydate = date("F jS", $unixdate);
+      //Get name or mailbox name is none
+      if (isset($header->from[0]->personal)) {
         $personal = $header->from[0]->personal;
-    } else {
+      } else {
         $personal = $header->from[0]->mailbox;
-    }
-    //Who it's from
-    $email = "$personal <{$header->from[0]->mailbox}@{$header->from[0]->host}>";
-    //display date, author and message
-    echo "$prettydate, $email \"$body\"\n";
+      }
+      //Who it's from
+      $email = "$personal <{$header->from[0]->mailbox}@{$header->from[0]->host}>";
+      //display date, author and message
+      echo "$prettydate, $email \"$body\"\n";
     }
   }
   imap_close($imap);
 ?>
-
 
     <div class="date-day"></div>
 
